6b12f779e06115bf4b1472cad2ef2806f0bb3c6a
liufeng2015 <liufeng_2015@163.com>
2016-05-17 18:26:59 +0800

    1111

--- directive/tablePlugins/LFTable.js
+++ directive/tablePlugins/LFTable.js
@@ -7,17 +7,24 @@
         .directive("lfTable",['$compile',function($compile){
             var templateDivs = "<div class = 'lf_container'>";
             templateDivs += "<div class = 'lf_head'>";
-            templateDivs += "<div class='lf_rowInput'><span>rows:</span><input type='text' ng-model='rows' placeholder='请输入'></div>";
-            templateDivs += "<div class='lf_search'><span>search:</span><input type='text' ng-model='search' placeholder='请输入'></div>";
+            templateDivs += "<div class='lf_rowInput'><span>rows:</span><input type='text' ng-model='tableConfig.rows' placeholder='please input'></div>";
+            templateDivs += "<div class='lf_search'><span>search:</span><input type='text' ng-model='search' placeholder='please input'></div>";
             templateDivs += "</div>";
             templateDivs += "<table class= 'lf_table'>";
             templateDivs += "<thead><tr>";
-            templateDivs += "<th ng-repeat = 'head in tableConfig.headList' ng-bind='head.key'><th>";
+            templateDivs += "<th class='bg-info' ng-repeat = 'head in tableConfig.headList' ng-bind='head.k'></th>";
             templateDivs += "</tr></thead>";
-            templateDivs += "<tbody>";
-            templateDivs += "<tr ng-repeat='itemArr in tableConfig.itemList'><td ng-repeat='item in itemArr' ng-bind='item'></td></tr>";
+            templateDivs += "<div class = 'lf_tbody_border'>";
+            templateDivs += "<tbody ng-repeat='itemArr in itemList'>";
+            templateDivs += "<tr ng-repeat='itemArr2 in itemArr'><td ng-repeat='item in itemArr2' ng-bind='item'></td></tr>";
             templateDivs += "</tbody>";
+            templateDivs += "</div>";
             templateDivs += "</table>";
+            templateDivs += "<div class ='lf_tablebottom'>";
+            templateDivs += "<div class= 'lf_right'><div class='lf_next_step glyphicon glyphicon-step-forward'></div><div class='lf_next_page glyphicon glyphicon-fast-forward'></div></div>";
+            templateDivs += "<div class= 'lf_center'><div class='lf_single_step'></div></div>";
+            templateDivs += "<div class= 'lf_left'><div class='lf_previous_page glyphicon glyphicon-fast-backward' ></div><div class='lf_previous_step glyphicon glyphicon-step-backward'></div></div>";
+            templateDivs += "</div>";
             templateDivs += "</div>";
             return{
                 restrict:"EA",
@@ -25,32 +32,25 @@
                 scope:{
                     tableConfig:"="
                 },
-                //compile:function(scope,element){
-                //    return{
-                //        pre:function(scope,element){
-                //            //var parentNode = angular.element(element[0]);
-                //            //var divs = "<side-item ng-repeat='item in sideData' item-data='item'></side-item>";
-                //            //var template = angular.element(divs);
-                //            //var newHtml = $compile(template)(scope);
-                //            //parentNode.append(newHtml);
-                //        }
-                //    }
-                //},
-                controllerAs:"sideListCtrl",
+                controllerAs:"lfTableCtrl",
                 controller:function($scope,$element){
-                    this.group = [];
-                    var self = this;
-                    this.closeOtherItem = function(currentScope){
-                        angular.forEach(self.group,function(nowScope){
-                            if(nowScope != currentScope){
-                                nowScope.isHide = true;
-                            }
-                        })
-                    };
+                    //$scope.
+                    var itemList = $scope.tableConfig.itemList;
+                    var rows = $scope.tableConfig.rows;
+                    var num = Math.ceil(itemList.length/rows);
+                    $scope.itemList = [];
+                    var arr = [];
+                    for (var i = 0; i < itemList.length;i++){
+                        //if()
 
+                    }
+                    //for (var i = 0; i < num;i++){
+                    //
+                    //}
+                    $scope.itemList = arr;
                 }
             }
-        }])
+        }]);
         //.directive("sideItem",['$compile',function($compile){
         //    return{
         //        restrict:'E',
